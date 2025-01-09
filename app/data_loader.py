1c0587f4c20f047ee1de085fbfcafac23a2fa59e
Mateusz Maciejewski <mateusz.maciejewski2001@gmail.com>
2025-01-10 00:41:56 +0100

    connected two datasets

--- app/data_loader.py
+++ app/data_loader.py
@@ -4,6 +4,7 @@ import os
 
 
 def detect_encoding(file_path):
+    """Detect the encoding of a file."""
     with open(file_path, "rb") as f:
         result = chardet.detect(f.read())
         return result['encoding']
@@ -13,15 +14,19 @@ class DataLoader:
 
     def __init__(self):
         self.spotify_top10s_path = "C:/Users/mateu/Documents/GitHub/SztucznaInteligencjaa/data/Spotify_top10s.csv"
+        self.spotify_2000s_path = "C:/Users/mateu/Documents/GitHub/SztucznaInteligencjaa/data/Spotify_2000s.csv"
 
     def load_datasets(self):
         if not os.path.exists(self.spotify_top10s_path):
             raise FileNotFoundError(f"File not found: {self.spotify_top10s_path}")
+        if not os.path.exists(self.spotify_2000s_path):
+            raise FileNotFoundError(f"File not found: {self.spotify_2000s_path}")
 
-        encoding = detect_encoding(self.spotify_top10s_path)
-        print(f"Detected encoding: {encoding}")
+        top10s_encoding = detect_encoding(self.spotify_top10s_path)
+        spotify_2000s_encoding = detect_encoding(self.spotify_2000s_path)
 
-        spotify_top10s = pd.read_csv(self.spotify_top10s_path, encoding=encoding)
+        spotify_top10s = pd.read_csv(self.spotify_top10s_path, encoding=top10s_encoding)
+        spotify_2000s = pd.read_csv(self.spotify_2000s_path, encoding=spotify_2000s_encoding)
 
         spotify_top10s = spotify_top10s.rename(columns={
             "title": "title",
@@ -37,4 +42,25 @@ class DataLoader:
             "pop": "popularity"
         })
 
-        return spotify_top10s
+        spotify_2000s = spotify_2000s.rename(columns={
+            "Title": "title",
+            "Artist": "artist",
+            "Top Genre": "top genre",
+            "Year": "year",
+            "Beats Per Minute (BPM)": "tempo",
+            "Energy": "energy",
+            "Danceability": "danceability",
+            "Loudness (dB)": "loudness",
+            "Liveness": "liveness",
+            "Valence": "valence",
+            "Length (Duration)": "duration_ms",
+            "Acousticness": "acousticness",
+            "Speechiness": "speechiness",
+            "Popularity": "popularity"
+        })
+
+        combined_data = pd.concat([spotify_top10s, spotify_2000s], ignore_index=True)
+
+        combined_data = combined_data.drop_duplicates(subset="title").reset_index(drop=True)
+
+        return combined_data
